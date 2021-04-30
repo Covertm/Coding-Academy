1e8b68c29806e8c573089b608635259140ff2fec
Covertm <mitchell.covert@nelnet.net>
2021-04-30 08:46:50 -0500

    04/30/21@08:47

--- Module 3/integers.js
+++ Module 3/integers.js
@@ -3,6 +3,10 @@ function ResetScreen() {
     tableRef.innerHTML = " ";
     var tableRef = document.getElementById("Mean")
     tableRef.innerHTML = " ";
+    var tableRef = document.getElementById("median")
+    tableRef.innerHTML = " ";
+    var tableRef = document.getElementById("modes")
+    tableRef.innerHTML = " ";
     document.getElementById('minNum').disabled=false;
     document.getElementById('maxNum').disabled=false;
     document.forms["myForm"]["InList"].value = "";
@@ -59,6 +63,9 @@ function AddToList() {
         var rows = tableref.rows.length;
         var sum = 0;
 
+//************************************************************** */    
+//*****************Mean***************************************** */    
+//************************************************************** */  
         for (var i=0; i < rows; i++) {
             sum += parseInt((tableref.rows[i].innerHTML));
         }
@@ -67,13 +74,57 @@ function AddToList() {
 
 //        tableItem = parseInt((tableref.rows[0].innerHTML));
         document.getElementById("Mean").innerHTML = average;
+
+
+//************************************************************** */    
+//*****************Median*************************************** */    
+//************************************************************** */    
+    var numbers=[];
+ 
+    for (var i=0; i < rows; i++) {
+        numbers.push(parseInt(tableref.rows[i].innerHTML));
     }
+    
+    var median = 0, numsLen = numbers.length;
+    numbers.sort();
+         
+    if (numsLen % 2 === 0) { // is even
+        median = (numbers[numsLen / 2 - 1] + numbers[numsLen / 2]) / 2; // average of two middle numbers
+    } else { // is odd
+        median = numbers[(numsLen - 1) / 2]; // middle number only
+    }
+
+    document.getElementById("median").innerHTML = median;
+
+//************************************************************** */    
+//*****************MODE***************************************** */    
+//************************************************************** */   
+    var modes = [], count = [], i, number, maxIndex = 0;
+ 
+    for (i = 0; i < numbers.length; i += 1) {
+        number = numbers[i];
+        count[number] = (count[number] || 0) + 1;
+        if (count[number] > maxIndex) {
+            maxIndex = count[number];
+        }
+    }
+
+    for (i in count)
+        if (count.hasOwnProperty(i)) {
+           if (count[i] === maxIndex) {
+            modes.push(Number(i));
+        }
+    }
+    var mode = modes.toString();
+
+    document.getElementById("modes").innerHTML = mode;
+
+}
+
     else{
         alert("You Must enter a number between Min and Max!");
         document.forms["myForm"]["InList"].value = "";
     }
 
     return;
-
-
 }
